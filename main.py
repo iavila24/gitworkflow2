88d7dec8f3baa06eb8b6f831ea5f6a7327f0e74e
VictorDolores64 <antonio.arteaga@uabc.edu.mx>
2023-05-23 19:56:13 -0700

    Se hicieron cambios

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
 age = 30
 name = "Profe 🎃"
-<<<<<<< HEAD
+
 print('Hola ' + name + ', tienes ' + str(age) + "años!!")
-=======
-print(f'Hola {name} tienes {str(age)} años!!')
 
-
-A = 0
-
-b = 0
-
-C = 0
-
-d = 0
->>>>>>> 850d5f1f6bc40358574ff4d2185b797e58c6b09f
