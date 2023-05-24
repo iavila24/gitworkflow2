7ac5b80806861d9131f76c914235c6417d5488e3
Nestor <delarosa.nestor@uabc.edu.mx>
2023-05-23 19:59:59 -0700

    a3

--- main.py
+++ main.py
@@ -36,7 +36,9 @@ entrada = input("¿En que esta pensandoo joven?: ")
 cantidad_palabras = len(entrada.split())
 
 print("Veamos, lo que dijo tiene ", cantidad_palabras, "palabras en total.")
-
+def imprimir_dibujo_ascii(dibujo1):
+    print(dibujo1)
+dibujo1=
 '''
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡠⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⢀⠀⠀⢀⣠⣴⣿⣿⡇⣀⣼⣾⡾⠀⢀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣾⣿⣿⡿⣟⣯⣿⣽⣾⣟⣷⣿⣽⣾⢿⣽⣾⢿⣽⣷⡿⣿⣿⣿⣶⡠⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣾⣿⣿⣤⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⡞⢠⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
